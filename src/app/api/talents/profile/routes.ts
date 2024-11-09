47af96047a38ae1b53b6f75781e44a53c9a2b6bd
Jamie Socorro <jamie_socorro@live.com>
2024-11-09 17:52:28 +0800

    feat: deleted api

--- src/app/api/talents/profile/routes.ts
+++
@@ -1,9 +0,0 @@
-// import { TalentModel } from "@/app/models/talent.model";
-
-// export async function POST() {
-//   return Response.json({
-//     name: "Dea",
-//     genre: "Rock",
-//     image: "images/Dea.jpg",
-//   } as TalentModel);
-// }
