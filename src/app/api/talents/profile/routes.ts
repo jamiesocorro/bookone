4c6355c99f4ffcbb81d33da22a830d40dd3a1b01
Jamie Socorro <jamie_socorro@live.com>
2024-11-09 17:51:11 +0800

    feat: commented

--- src/app/api/talents/profile/routes.ts
+++ src/app/api/talents/profile/routes.ts
@@ -1,9 +1,9 @@
-import { TalentModel } from "@/app/models/talent.model";
+// import { TalentModel } from "@/app/models/talent.model";
 
-export async function POST() {
-  return Response.json({
-    name: "Dea",
-    genre: "Rock",
-    image: "images/Dea.jpg",
-  } as TalentModel);
-}
+// export async function POST() {
+//   return Response.json({
+//     name: "Dea",
+//     genre: "Rock",
+//     image: "images/Dea.jpg",
+//   } as TalentModel);
+// }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,3 +1,3 @@
-export async function GET() {
-  return Response.json({ message: "test jamie" });
-}
+// export async function GET() {
+//   return Response.json({ message: "test jamie" });
+// }
