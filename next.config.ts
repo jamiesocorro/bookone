9a7a34c859b614803c7beaf278a431dfb80014af
Jamie Socorro <jamie_socorro@live.com>
2024-11-09 18:04:03 +0800

    feat: updated config

--- next.config.ts
+++ next.config.ts
@@ -1,52 +1,60 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: "export",
+// };
+
+// export default nextConfig;
+
+// module.exports = {
+//   images: {
+//     domains: ["localhost", "storage.googleapis.com", "flagcdn.com"],
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         headers: [
+//           {
+//             key: "X-Content-Type-Options",
+//             value: "nosniff",
+//           },
+//           {
+//             key: "X-Frame-Options",
+//             value: "DENY",
+//           },
+//           {
+//             key: "Referrer-Policy",
+//             value: "strict-origin-when-cross-origin",
+//           },
+//         ],
+//       },
+//       {
+//         source: "/sw.js",
+//         headers: [
+//           {
+//             key: "Content-Type",
+//             value: "application/javascript; charset=utf-8",
+//           },
+//           {
+//             key: "Cache-Control",
+//             value: "no-cache, no-store, must-revalidate",
+//           },
+//           {
+//             key: "Content-Security-Policy",
+//             value: "default-src 'self'; script-src 'self'",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "exports", // lightweight but no dynamic routes & api support
+  // output: "standalone", // bulkier but supports dynamic routes & api
 };
 
-export default nextConfig;
-
-module.exports = {
-  images: {
-    domains: ["localhost", "storage.googleapis.com", "flagcdn.com"],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-        ],
-      },
-      {
-        source: "/sw.js",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/javascript; charset=utf-8",
-          },
-          {
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
-};
+module.exports = nextConfig;
